bd3d45cd117f7c6568e5fabd711af90c39bd3aeb
Sinde530 <70146617+sinde530@users.noreply.github.com>
2025-03-11 21:06:49 +0900

    Create useMobile.ts

---
+++ src/hooks/useMobile.ts
@@ -0,0 +1,25 @@
+'use client';
+
+import { useState, useEffect } from 'react';
+
+export function useMobile() {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const checkIfMobile = () => {
+        setIsMobile(window.innerWidth < 768);
+      };
+
+      checkIfMobile();
+
+      window.addEventListener('resize', checkIfMobile);
+
+      return () => {
+        window.removeEventListener('resize', checkIfMobile);
+      };
+    }
+  }, []);
+
+  return isMobile;
+}
